e92b7d8633d2937a33ee4961decc91aef73a5e90
becario-emtusa <contacto@franbeltran.es>
2024-05-24 01:49:42 +0200

    ✨ Update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  name: 'TFG-WEB',
-
-  // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-
-  script: 'npm run build && npm run start',
-  watch: true,
-  exec_mode: 'cluster',
-  instances: 'max'
+  apps: [
+    {
+      name: 'TFG-WEB',
+      script: 'npm run build && npm run start',
+      watch: true,
+      exec_mode: 'cluster',
+      instances: 'max'
+    }
+  ]
 }
