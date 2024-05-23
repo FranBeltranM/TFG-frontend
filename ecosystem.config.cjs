5073350dabad12b599467539acc5a1b8048fc54f
becario-emtusa <contacto@franbeltran.es>
2024-05-24 01:59:58 +0200

    🔨 Correct ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,9 +2,18 @@ module.exports = {
   apps: [
     {
       name: 'TFG-WEB',
-      script: 'npm run start',
-      exec_mode: 'cluster',
-      instances: 'max'
-    }
-  ]
-}
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
+      instances: 'max',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
